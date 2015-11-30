126d419e7076253d889a8a97c1bb1a446304a7c2
= <=>
2015-11-30 15:49:38 +0700

    cleaning

--- Coursera/Various/FindDuplicate.java
+++ Coursera/Various/FindDuplicate.java
@@ -1,3 +1,6 @@
+//Find a duplicate. Given an array of N elements in which each element is an integer between 1 and N, 
+//write an algorithm to determine if there are any duplicates. Your algorithm should run in linear time 
+//and use O(1) extra space. Hint: you may destroy the array.
 
 public class FindDuplicate {
 
