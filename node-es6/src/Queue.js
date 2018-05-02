56e09a6cfc04c6d6c5c347f279f107557e8196cc
GA1 <nvkga1@gmail.com>
2018-05-03 00:29:06 +0200

    Add dequeue

--- node-es6/src/Queue.js
+++ node-es6/src/Queue.js
@@ -11,6 +11,13 @@ Queue.prototype.size = function() {
   return this.data.length;
 }
 
+Queue.prototype.pop = function() {
+  return this.data[this.data.length - 1]
+}
+
+Queue.prototype.dequeue = function() {
+  return this.data.shift()
+}
+
+
 module.exports.Queue = Queue;
-// module.exports.size = size;
-// module.exports.Queue = Queue;
--- node-es6/src/__test__/Queue.test.js
+++ node-es6/src/__test__/Queue.test.js
@@ -5,8 +5,43 @@ test('empty queue is of size 0', () => {
   expect(q.size()).toEqual(0);
 });
 
-test('adding 1 element to an empty queue makes size equals 1', () => {
+test('enqueue 1 element to an empty queue, check size equals 1', () => {
   var q = new x.Queue()
   q.enqueue(7)
   expect(q.size()).toEqual(1);
 });
+
+test('enqueue 2 elements to an empty queue, check size equals 2', () => {
+  var q = new x.Queue()
+  q.enqueue(7)
+  q.enqueue(11)
+  expect(q.size()).toEqual(2);
+})
+
+test('enqueue 1 element, pop the element', () => {
+  var q = new x.Queue()
+  q.enqueue(7)
+  expect(q.pop()).toEqual(7);
+})
+
+test('enqueue 2 elements, pop the last element', () => {
+  var q = new x.Queue()
+  q.enqueue(7)
+  q.enqueue(11)
+  expect(q.pop()).toEqual(11);
+})
+
+
+test('enqueue 1 element, dequeue the element', () => {
+  var q = new x.Queue()
+  q.enqueue(7)
+  expect(q.dequeue()).toEqual(7);
+})
+
+test('enqueue 2 elements, dequeue both elements', () => {
+  var q = new x.Queue()
+  q.enqueue(2)
+  q.enqueue(5)
+  expect(q.dequeue()).toEqual(2);
+  expect(q.dequeue()).toEqual(5);
+})
